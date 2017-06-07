d1c548ce6bc078e3ff026b019e63a2c584735968
bvasiljevic <brendavasiljevicsm@gmail.com>
2017-06-07 01:59:34 +0000

    update matcher to interface

--- api/modules/v1/models/Matcher.php
+++ api/modules/v1/models/Matcher.php
@@ -3,51 +3,24 @@
 namespace api\modules\v1\models;
 
 /**
- * Matcher1 - matcher for one match
+ * Matcher - methods match compatibility definition
  *
- * This class provides the functions implementation for single return matcher
+ * This interface defines which matches will be presented to the user
  *
  */
- 
- class Matcher1 implements Matcher
- {
-     /**
-     * Match 
-     * Returns an array with the best (single) match
+interface Matcher
+{
+    /*!
+     * Match
+     * Returns an array of matches
      * 
      * @param item => item to be matched
      * @param itemList => possible matches
      *
      * return array
      *
+     * @since 0.1
+     * @access public
     */
-     public function match($item, $itemList)
-     {
-         $best_compatibility = 0;
-         
-         foreach ($itemList as $itemB)
-         {
-            $compatibility = 0;
-            $i = 0;
-            $n = sizeof($item); //Number of attributes
-            $match_comparator = new MatchComparator();
-            
-            //Compares the items attribute per attribute; compatibility receives the sum of attribute similarity
-            while($i < $n){
-                $compatatibility += $match_comparator->compareAttribute($item[i], $itemB[i]);
-                $i++;
-            }
-           
-           //Sum of attribute similarity between the items is divided by the number of attributes, resulting in a number between 0 and 1 (%)
-           $compatibility = $compatibility/$n;
-           
-           //If the current item is the most compatible so far, it is saved in match
-           if($compatibility > $best_compatibility){
-               $best_compatibility = $compatibility;
-               $match = $itemB;
-           }
-         }
-         
-         return $match;
-     }
- }
+    public function match($item, $itemsList);
+}
