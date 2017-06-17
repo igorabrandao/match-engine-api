eada0fbbd305630ed4f14534ee005987956f53eb
bvasiljevic <brendavasiljevicsm@gmail.com>
2017-06-17 22:35:57 +0000

    Commentary clarification

--- api/modules/v1/matchEngine/DecisionMaker.php
+++ api/modules/v1/matchEngine/DecisionMaker.php
@@ -10,14 +10,14 @@ namespace api\modules\v1\matchEngine;
 interface DecisionMaker
 {
     /**
-     * This method define how the match should be accepted
+     * This method define the behavior of the system should the match be accepted
      *
      * @return mixed
      */
     public function acceptMatch();
 
     /**
-     * This method define how the match should be rejected
+     * This method define the behavior of the system should the match be rejected
      *
      * @return mixed
      */
